9237c33d796bb0231180415516aff13c91cb38a3
favsol26 <favsol26@gmail.com>
2017-12-08 02:17:11 -0400

    creando el proyecto final

--- Proyecto final/resources/js/estudiante.js
+++ Proyecto final/resources/js/estudiante.js
@@ -7,65 +7,52 @@ var Estudiante = function () {
   self.telefonoe = "";
   self.emaile = "";
 };
-
 $(document).ready(function () {
-
   myStorage = window.localStorage;
   var dbEstudiantes = myStorage.getItem("Estudiantes");
-
   if (dbEstudiantes != null) {
-
     var Estudiantes = JSON.parse(dbEstudiantes);
-
     $.each(Estudiantes, function (i, est) {
       addrow(est);
-    }); 
+    });
   }
-
-  $("#addEst").click(function () {
-    // alert("HOLA!!!");
+  $("#addEst").click(function () { // alert("HOLA!!!");
     var id = $("#IdEst").val();
     var nombre = $("#nombreEst").val();
     var matricula = $("#matriculaEst").val();
     var identificacion = $("#identificacionEst").val();
     var telefono = $("#telefonoEst").val();
     var email = $("#emailEst").val();
-
     var est1 = new Estudiante();
-
     est1.ide = id;
     est1.nombree = nombre;
     est1.matriculae = matricula;
     est1.identificacione = identificacion;
     est1.telefonoe = telefono;
-    est1.emaile = email; 
-    // alert(JSON.stringify(est1));
-    addrow(est1);
+    est1.emaile = email;
+    addrow(est1);// alert(JSON.stringify(est1));
     guardardb(est1);
   });
-
-
   $("#delEst").click(function () {
     var con = 0;
-    var f1 = 0;
+    var f1 = [];
     $("input:checkbox:checked").each(function () {
-      f1 = $(this).attr("id");
+      f1[con] = $(this).attr("id");
       con++;
     });
-
-    if (con == 1) {
-      eliminarEstudiante(f1);
+    if (con >= 1) {
+      $.each(f1, function (i, est) {
+        eliminarEstudiante(est - i);
+      });
+      location.reload();
     } else {
-      alert("debe elegir 1");
+      alert("debe elegir al menos 1");
     }
-  }); 
+  });
 });
-
 function addrow(datos) {
-
   var id = document.getElementById("estudiantes").rows.length;
   console.log(id);
-
   var rowDatos = "<tr>"
     + '<td> <input type="checkbox" id ="' + id + '"></td>'
     + "<td>" + datos.ide + "</td>"
@@ -75,46 +62,33 @@ function addrow(datos) {
     + "<td>" + datos.telefonoe + "</td>"
     + "<td>" + datos.emaile + "</td>"
     + " </tr>";
-
   $("table tbody").append(rowDatos);
-
 };
-
-
-
 function guardardb(datos) {
   // buscando el controlador localStorage
   myStorage = window.localStorage;
-
   var Estudiantes = [];
-
   var dbEstudiantes = myStorage.getItem("Estudiantes");
   if (dbEstudiantes != null) {
     Estudiantes = JSON.parse(dbEstudiantes);
   }
-
   Estudiantes.push(datos);
   myStorage.setItem("Estudiantes", JSON.stringify(Estudiantes));
 };
-
 function eliminarEstudiante(fila) {
-  // alert(fila);
   myStorage = window.localStorage;
   var sTextoBuscado = "{";
-  var expression = fila;
+  var expression = 0;
   var row = fila;
   var controlador = 0;
   var contador = 0;
   var datosAnteriores = myStorage.getItem("Estudiantes");
-
   if (datosAnteriores != null) {
     var sTexto = datosAnteriores;
-
     while (sTexto.indexOf(sTextoBuscado) > -1) {
       sTexto = sTexto.substring(sTexto.indexOf(sTextoBuscado) + sTextoBuscado.length, sTexto.length);
       contador++;
     }
-
     for (var i = 0; i <= datosAnteriores.length; i++) {
       if (datosAnteriores.substring(i, i + 1) == '{') {
         controlador++;
@@ -124,14 +98,13 @@ function eliminarEstudiante(fila) {
         }
       }
     }
-
     var start = datosAnteriores.indexOf("{", fila);
     var end = datosAnteriores.indexOf("}", start);
-
-    if (expression == contador && contador > 1) {
+    if (row == contador && contador > 1) {
       expression = 1;
+    } else {
+      expression = 2;
     }
-
     switch (expression) {
       case 1:
         var datosTemporales = (datosAnteriores.substring(start - 1, end + 1));
@@ -139,7 +112,6 @@ function eliminarEstudiante(fila) {
       default:
         var datosTemporales = (datosAnteriores.substring(start, end + 2));
     }
-
     if (contador > 1) {
       var datosNuevos = datosAnteriores.replace(datosTemporales, "");
       myStorage.removeItem("Estudiantes");
@@ -148,8 +120,5 @@ function eliminarEstudiante(fila) {
     } else {
       myStorage.removeItem("Estudiantes");
     }
-    // console.log(datosAnteriores);
-    // console.log(datosNuevos);
   }
-  location.reload();
 };
